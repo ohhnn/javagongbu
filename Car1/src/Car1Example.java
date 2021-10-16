15254918cb7a8d0c93da180dea944e504d0cca8d
ohhnn <90627658+ohhnn@users.noreply.github.com>
2021-10-17 00:33:46 +0900

    Add files via upload
    return문

---
+++ Car1/src/Car1Example.java
@@ -0,0 +1,23 @@
+
+public class Car1Example {
+
+	public static void main(String[] args) {
+		Car1 myCar = new Car1();
+		
+		myCar.setGas(5); 
+		
+		boolean gasState = myCar.isLeftGas();
+		if(gasState) {
+			System.out.println("출발합니다.");
+			myCar.run();
+		}
+		
+		if(myCar.isLeftGas()) {
+			System.out.println("gas를 주입할 필요가 없습니다.");
+		} else {
+			System.out.println("gas를 주입하세요.");
+		}
+
+	}
+
+}
